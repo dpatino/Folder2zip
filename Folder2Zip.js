68a2fdc0ce3d9ac7c687a2bf17638dd3cfacc7f8
Diego Orlando Patino Ramos <d.patino@bee-buzziness.com>
2015-07-08 12:01:46 +0200

    change promise resolve

--- Folder2Zip.js
+++ Folder2Zip.js
@@ -1,10 +1,9 @@
 /**
  * Created by d.patino on 02/07/2015.
  */
-var debug = require('debug')
+var debug = require('debug')('Folder2Zip')
 var util = require("util");
 var events = require('events');
-
 var Promise = require('bluebird');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
@@ -22,23 +21,15 @@ Folder2Zip = (function () {
 	 */
 
 	function Folder2Zip(working_folder, source_path, folder_name) {
+		this.processOptions = {
+			cwd: working_folder
+		};
 		if (source_path != null && folder_name != null) {
-			debug('Folder2Zip : add ' + source_path + '/' + folder_name)
+			debug('init: add ' + working_folder + '/' + source_path + " to " + folder_name);
 			findCommand = 'find ./' + source_path + ' |wc -l';
-			zipCommand = ['-r', folder_name, source_path]
-			testZipCommand = ['-T', folder_name + '.zip']
-			var self = this;
-			process.chdir(working_folder)
-			debug("Working folder :" + process.cwd())
-			exec(findCommand, function (error, stdout, stderr) {
-				if (stdout) {
-					self.filesLength = Number(stdout)
-					self.emit('ready');
-					debug('Folder2Zip ready ; ' + self.filesLength + ' files to compress')
-				}
-				if (error) debug('ERROR : ' + error);
-				if (stderr)  debug('stderr : ' + stderr);
-			})
+			zipCommand = ['-r', folder_name, source_path ];
+			testZipCommand = ['-T', folder_name ];
+			debug("working folder :" + working_folder);
 		}
 	}
 
@@ -47,74 +38,82 @@ Folder2Zip = (function () {
 		var self = this;
 		return new Promise(function (resolve, reject) {
 			self.on("zipEnd", function (data) {
-				resolve(data)
-			})
+				resolve(data);
+			});
 			self.on("error", function (data) {
-				reject(data)
-			})
+				reject(data);
+			});
 			self.on('ready', function () {
-				self.zip()
-			})
-			exec(findCommand, function (error, stdout, stderr) {
-				if (stdout) {
-					self.filesLength = Number(stdout)
-					self.emit('ready');
-					debug('Folder2Zip.zipPromise ready ; ' + self.filesLength + ' files to compress')
+			});
+			exec(findCommand, self.processOptions, function (error, stdout, stderr) {
+				if (error) {
+					debug('findCommand (2) error: ' + error);
+					return;
 				}
-				if (error)    reject(error);
-				if (stderr)    reject(stderr);
+				if (stderr) {
+					debug('findCommand (2) stderr: ' + stderr);
+					return;
+				}
+				if (stdout) {
+					self.filesLength = Number(stdout);
+					self.zip();
+					debug('zipPromise ready ; ' + self.filesLength + ' files to compress')
+				} else {
+					debug('findCommand (2) no stdout? ' + stdout);
+				}
 			})
 		})
-	}
+	};
 	Folder2Zip.prototype.zip = function () {
-		this.startZip()
-	}
+		debug('#zip');
+		this.startZip();
+	};
+	Folder2Zip.prototype.processOptions = null;
 	Folder2Zip.prototype.filesLength = null;
 	Folder2Zip.prototype.startZip = function () {
+		debug('#startZip');
 		var self = this;
 		var bar = new ProgressBar(':bar', {total: this.filesLength, complete: "=", width: 20, incomplete: ""});
-		var zip = spawn('zip', zipCommand, {cwd: null})
+		var zip = spawn('zip', zipCommand, this.processOptions);
+		debug("zipcommand := " + zipCommand);
 		zip.stdout.on('data', function (data) {
 			bar.tick();
+//			debug("zip.stdout.on('data') : "  +data)
 			self.emit('progress', bar.width);
 		});
 		zip.stderr.on('data', function (error) {
 			self.emit('error', error);
-			console.log('ps stderr: ' + error);
+			debug("zip.stderr.on('data') : "  +error)
 		});
 		zip.on('close', function (code) {
-			bar.tick()
-			self.testZip()
+			debug('zip close ' + code);
+			bar.tick();
+			self.testZip();
 		});
-	}
+	};
 	Folder2Zip.prototype.testZip = function () {
 		var self = this;
 		var zipTestResult = false;
-		var zipTest = spawn('zip', testZipCommand, {cwd: null})
+		var zipTest = spawn('zip', testZipCommand, this.processOptions);
 		zipTest.stdout.on('data', function (data) {
-			var dataString = data.toString().toLowerCase()
-			if (dataString.indexOf('ok') != -1) {
-				zipTestResult = true;
-			} else {
-				zipTestResult = false;
-			}
-			debug('testZip : ' + dataString)
+			var dataString = data.toString().toLowerCase();
+			zipTestResult = dataString.indexOf('ok') != -1;
+			debug('zipTest.stdout.on : ' + dataString)
 		});
 		zipTest.stderr.on('data', function (error) {
+			debug("zipTest.stderr  ERROR : " + error);
 			self.emit('error', error);
 		});
 		zipTest.on('close', function (code) {
-			debug('testzip close ' + code)
-			if (zipTestResult == true && code == 0) {
-				process.chdir(__dirname)
+			debug('testzip close ' + code);
+			if (zipTestResult && code == 0) {
 				self.emit('zipEnd', '100');
 			} else {
-				process.chdir(__dirname)
 				self.emit('error', {'code': code});
 			}
-
 		});
-	}
-	return Folder2Zip
-})()
-module.exports = Folder2Zip
+	};
+
+	return Folder2Zip;
+})();
+module.exports = Folder2Zip;
